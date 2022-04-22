3ab6119e9a3c6b2865dba855a384e475679cd566
Jianfeng0Liu <104093135+Jianfeng0Liu@users.noreply.github.com>
2022-04-22 15:03:49 +0200

    Update CalibrationGraph.py

--- CalibrationGraph.py
+++ CalibrationGraph.py
@@ -23,3 +23,22 @@ for i in [1,2,3,5]:
     plt.xlabel('Channel')
     plt.ylabel('Luminance[cd/m^2]')
     plt.show
+   
+#curvefitting
+def ChannelValue(l:int, perc:float): #l:light(R-1,G-2,B-3,A-5), perc:percentage of the channel
+    if l == 1:
+        para = np.array([3.79101032e-03, -7.71247848e-02,9.17950337e+00])
+        func = np.poly1d(para)
+    elif l == 2:
+        para = np.array([9.14054992e-03, -2.08932099e-01,2.49404590e+01])
+        func = np.poly1d(para)
+    elif l == 3:
+        para = np.array([1.76936023e-03, -4.70784519e-02,4.83069050e+00])
+        func = np.poly1d(para)
+    elif l == 5:
+        para = np.array([3.47076694e-03, -2.68425662e-02,8.62810845e+00])
+        func = np.poly1d(para)
+    
+    return int([x for x in np.roots(func-perc*255) if x > 0][0])
+
+print(ChannelValue(1,0.9))
